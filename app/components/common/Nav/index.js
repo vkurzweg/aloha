bb3d37288f7664d576629991556593fac4899dd4
Victoria Dunham <vkurzweg@gmail.com>
2017-04-28 15:16:36 -0700

    add navbar

---
+++ app/components/common/Nav/index.js
@@ -0,0 +1,96 @@
+/**
+*
+* Nav
+*
+*/
+
+import React from 'react';
+import styled from 'styled-components';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import { browserHistory } from 'react-router';
+import Menu from 'assets/icons/menu.png';
+import Yelp from 'assets/icons/yelp.png';
+import Tripadvisor from 'assets/icons/tripadvisor.png';
+import Thumbtack from 'assets/icons/thumbtack.png';
+import Facebook from 'assets/icons/facebook.png';
+import Instagram from 'assets/icons/instagram.png';
+
+
+// const muiTheme = getMuiTheme({
+//   appPalette: {
+//     primary1Color: 'grey900',
+//   }
+// });
+
+const StyledAppBar = styled(AppBar)`
+  width: 100%;
+  background: #2bf7d0;
+  background: -moz-linear-gradient(left, #2bf7d0 0%, #8ae5ab 95%);
+  background: -webkit-linear-gradient(left, #2bf7d0 0%,#8ae5ab 95%);
+  background: linear-gradient(to right, #2bf7d0 0%,#8ae5ab 95%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2bf7d0', endColorstr='#8ae5ab',GradientType=1 );
+`;
+
+class Nav extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({open: !this.state.open})
+  }
+
+  handleClose() {
+    this.setState({open: false})
+  }
+
+  render() {
+    let display = 'none';
+    (this.state.showMenu) ? display = 'block' : display = 'none';
+    const icons = <div style={{ display: 'inline-flex', marginLeft: '-40%' }}>
+                    <img src={Yelp} alt="yelp icon" />
+                    <img src={Tripadvisor} alt="tripadvisor icon" style={{ paddingLeft: '15%' }} />
+                    <img src={Thumbtack} alt="thumbtack icon" style={{ paddingLeft: '15%' }} />
+                    <img src={Facebook} alt="facebook icon" style={{ paddingLeft: '15%' }} />
+                    <img src={Instagram} alt="instagram icon" style={{ paddingLeft: '15%' }} />
+                  </div>
+    const brand = <a href="/" style={{ textTransform: 'uppercase', fontSize: '16px', color: 'black', textDecoration: 'none' }}>Aloha Brothers Surf Lessons</a>
+    return (
+      <div>
+        <div style={{ position: 'fixed', width: '100%', zIndex: '10' }}>
+          <StyledAppBar
+              title={brand}
+              titleStyle={{ textDecoration: 'none' }}
+              iconElementLeft={<img src={Menu} alt="menu icon" />}
+              iconStyleLeft={{ padding: '1.3%'}}
+              onLeftIconButtonTouchTap={this.handleToggle}
+              iconElementRight={icons}
+              iconStyleRight={{ marginTop: '2%', marginRight: '1.5%'}}
+            />
+            <Drawer
+              docked={false}
+              width={200}
+              open={this.state.open}
+              onRequestChange={(open) => this.setState({ open })}
+            >
+              <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+              <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+            </Drawer>
+        </div>
+      </div>
+    );
+  }
+}
+
+Nav.propTypes = {
+
+};
+
+export default Nav;
