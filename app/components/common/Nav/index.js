5e1020144b0ab517d61281177f422d57f9fa71ba
Victoria Dunham <vkurzweg@gmail.com>
2017-04-28 17:57:37 -0700

    setup navigation

--- app/components/common/Nav/index.js
+++ app/components/common/Nav/index.js
@@ -9,7 +9,6 @@ import styled from 'styled-components';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
-import { browserHistory } from 'react-router';
 import Menu from 'assets/icons/menu.png';
 import Yelp from 'assets/icons/yelp.png';
 import Tripadvisor from 'assets/icons/tripadvisor.png';
@@ -33,6 +32,31 @@ const StyledAppBar = styled(AppBar)`
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#2bf7d0', endColorstr='#8ae5ab',GradientType=1 );
 `;
 
+const items = [
+  {
+    name: 'Rates & Booking',
+    url: '/booking',
+  }, {
+    name: 'Instructors',
+    url: '/instructors',
+  }, {
+    name: 'FAQ',
+    url: '/faq',
+  }, {
+    name: 'Gallery',
+    url: '/gallery',
+  }, {
+    name: 'Press',
+    url: '/press',
+  }, {
+    name: 'Retreats',
+    url: '/retreats',
+  }, {
+    name: 'Contact',
+    url: '/contact',
+  },
+];
+
 class Nav extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
@@ -47,42 +71,49 @@ class Nav extends React.Component { // eslint-disable-line react/prefer-stateles
     this.setState({open: !this.state.open})
   }
 
-  handleClose() {
-    this.setState({open: false})
+  handleClose(url, e) {
+    console.log(url, this.props.onClick)
+    this.setState({open: false});
+    this.props.onClick(url);
   }
 
   render() {
     let display = 'none';
     (this.state.showMenu) ? display = 'block' : display = 'none';
-    const icons = <div style={{ display: 'inline-flex', marginLeft: '-40%' }}>
-                    <img src={Yelp} alt="yelp icon" />
-                    <img src={Tripadvisor} alt="tripadvisor icon" style={{ paddingLeft: '15%' }} />
-                    <img src={Thumbtack} alt="thumbtack icon" style={{ paddingLeft: '15%' }} />
-                    <img src={Facebook} alt="facebook icon" style={{ paddingLeft: '15%' }} />
-                    <img src={Instagram} alt="instagram icon" style={{ paddingLeft: '15%' }} />
-                  </div>
-    const brand = <a href="/" style={{ textTransform: 'uppercase', fontSize: '16px', color: 'black', textDecoration: 'none' }}>Aloha Brothers Surf Lessons</a>
+    const icons = (
+      <div style={{ display: 'inline-flex', marginLeft: '-40%', width: '90%' }}>
+        <img src={Yelp} alt="yelp icon" />
+        <img src={Tripadvisor} alt="tripadvisor icon" style={{ paddingLeft: '15%' }} />
+        <img src={Thumbtack} alt="thumbtack icon" style={{ paddingLeft: '15%' }} />
+        <img src={Facebook} alt="facebook icon" style={{ paddingLeft: '15%' }} />
+        <img src={Instagram} alt="instagram icon" style={{ paddingLeft: '15%' }} />
+      </div>
+      );
+    const brand = <a href="/" style={{ textTransform: 'uppercase', fontSize: '16px', color: 'black', textDecoration: 'none' }}>Aloha Brothers Surf Lessons</a>;
     return (
       <div>
         <div style={{ position: 'fixed', width: '100%', zIndex: '10' }}>
           <StyledAppBar
-              title={brand}
-              titleStyle={{ textDecoration: 'none' }}
-              iconElementLeft={<img src={Menu} alt="menu icon" />}
-              iconStyleLeft={{ padding: '1.3%'}}
-              onLeftIconButtonTouchTap={this.handleToggle}
-              iconElementRight={icons}
-              iconStyleRight={{ marginTop: '2%', marginRight: '1.5%'}}
-            />
-            <Drawer
-              docked={false}
-              width={200}
-              open={this.state.open}
-              onRequestChange={(open) => this.setState({ open })}
-            >
-              <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
-              <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
-            </Drawer>
+            title={brand}
+            titleStyle={{ textDecoration: 'none' }}
+            iconElementLeft={<img src={Menu} alt="menu icon" />}
+            iconStyleLeft={{ padding: '1.3%' }}
+            onLeftIconButtonTouchTap={this.handleToggle}
+            iconElementRight={icons}
+            iconStyleRight={{ marginTop: '2%', marginRight: '1.5%' }}
+          />
+          <Drawer
+            docked={false}
+            width={200}
+            open={this.state.open}
+            onRequestChange={(open) => this.setState({ open })}
+          >
+            {items.map((item, idx) => {
+              return (
+                <MenuItem key={idx} onTouchTap={this.handleClose.bind(null, item.url)}>{item.name}</MenuItem>
+              );
+            })}
+          </Drawer>
         </div>
       </div>
     );
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,14 +10,37 @@
  */
 
 import React from 'react';
+import { connect } from 'react-redux';
 import Nav from 'components/common/Nav';
+import { push } from 'react-router-redux';
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.handleNav = this.handleNav.bind(this);
+  }
+
+  handleNav(e, url) {
+    this.props.moveLocation(url);
+  }
+
   render() {
     return (
       <div>
-        <Nav />
+        <Nav
+          onClick={this.handleNav}
+        />
       </div>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    moveLocation: (url) => dispatch(push(url)),
+  }
+};
+
+export default connect(null, mapDispatchToProps)(HomePage);
+
