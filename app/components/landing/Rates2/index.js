9c69f41d50e02d127dd64337edfd7fd155543600
Victoria Dunham <vkurzweg@gmail.com>
2017-06-02 11:52:19 -0700

    fix rates secton - landing

--- app/components/landing/Rates2/index.js
+++ app/components/landing/Rates2/index.js
@@ -29,28 +29,24 @@ function Rates() {
 
       <div className="container" style={{ backgroundColor: '#ECECEC', width: '100%' }}>
         <div className="row" style={{ marginTop: '5%' }}>
-          <div className="col-sm-6" style={{ display: 'block', margin: '0 auto', marginBottom: '2%' }}>
-            <Card style={{ display: 'block', margin: '0 auto', width: '60%', marginLeft: '25%' }}>
-              <CardMedia>
-                <Image className="item" cloudName="kurzweg" publicId="cuteness_overload_kkto2i" width="400" quality="auto" responsive />
-              </CardMedia>
-              <CardTitle title="Standard Group Lesson" subtitle="$85/person" />
-              <CardText style={{ textAlign: 'center', fontSize: '20px', }}>
+          <div className="col-sm-6" style={{ display: 'block', margin: '0 auto', position: 'relative', height: '0', paddingBottom: '50%' }}>
+            <Image className="item" cloudName="kurzweg" publicId="cuteness_overload_kkto2i" width="400" quality="auto" responsive />
+              <h3>Standard Group Lesson</h3>
+              <h4>$85/person</h4>
+              <CardText style={{ textAlign: 'center' }}>
                 1 instructor for up to 4 people
                 <hr style={{ marginTop: '2%', marginBottom: '2%' }} />
                 We create the group if you don't have at least 3 people
-                <hr style={{ marginTop: '2%', marginBottom: '2%' }} />
-                Most popular option!
               </CardText>
             </Card>
           </div>
-        <div className="col-sm-6" style={{ display: 'block', margin: '0 auto', marginBottom: '2%' }}>
-          <Card style={{ display: 'block', margin: '0 auto', width: '60%' }}>
+        <div className="col-sm-6" style={{ display: 'block', margin: '0 auto', position: 'relative', height: '0', paddingBottom: '50%' }}>
+          <Card style={{ position: 'absolute', display: 'block', margin: '0 auto', width: '60%' }}>
             <CardMedia>
-              <Image className="item" cloudName="kurzweg" publicId="duo2_ktikxn" width="400" quality="auto" responsive />
+              <Image className="item" cloudName="kurzweg" publicId="duo2_hlvkfo" width="400" quality="auto" responsive />
             </CardMedia>
             <CardTitle title="Private Lesson (2 ppl)" subtitle="$255"/>
-            <CardText style={{ textAlign: 'center', fontSize: '20px', marginBottom: '4%' }}>
+            <CardText style={{ textAlign: 'center' }}>
               1 instructor for 2 people
               <hr style={{ marginTop: '2%', marginBottom: '2%' }} />
               Perfect for couples or children and their parents!
@@ -62,7 +58,7 @@ function Rates() {
       <div style={{ backgroundColor: '#ECECEC' }}>
         <Link to="/gallery"><div style={{ border: '2px solid #FF80AB', width: '30%', margin: '0 auto', padding: '2%' }}>
           <Image cloudName="kurzweg" publicId="camera" width="auto" responsive alt="surf photography" style={{ display: 'block', margin: '0 auto' }} />
-          <p style={{ color: '#FF80AB', textAlign: 'center', fontSize: '22px', padding: '1%' }}>Add photography to your lesson for $75</p>
+          <p style={{ color: '#FF80AB', textAlign: 'center', fontSize: '16px', padding: '1%' }}>Add photography to your lesson for $75</p>
         </div></Link>
         <div style={{ marginTop: '3%', paddingBottom: '5%' }}>
           <Link style={{ textDecoration: 'none', cursor: 'pointer' }} to="/rates" ><Btn>All rates & packages</Btn></Link>
