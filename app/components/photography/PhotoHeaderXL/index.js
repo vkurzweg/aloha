8eb388fb1163cf9dd295a6bc15e271378569f23d
Victoria Dunham <vkurzweg@gmail.com>
2017-06-14 10:11:32 -0700

    edit photography page copy

--- app/components/photography/PhotoHeaderXL/index.js
+++ app/components/photography/PhotoHeaderXL/index.js
@@ -13,15 +13,31 @@ function PhotoHeaderXl() {
     <div>
       <h3 style={{ textAlign: 'center', color: '#7C4DFF', letterSpacing: '5px', textTransform: 'uppercase', paddingTop: '8%', fontSize: '28px' }}>Photography</h3>
       <div style={{ fontSize: '16px', fontFamily: 'Josefin Sans', width: '75%', margin: '0 auto', textAlign: 'center', marginTop: '2%' }}>
-        <p style={{ fontFamily: 'Josefin Sans', fontSize: '20px' }}>Add professional photography to your lesson</p>
+        <p style={{ fontFamily: 'Josefin Sans', fontSize: '18px' }}><b>Add professional photography to your lesson</b></p>
+        <ul style={{ width: '50%', display: 'block', margin: '0 auto', textAlign: 'left' }}>
+          <li style={{ fontFamily: 'Josefin Sans' }}>Your best images, edited & exported in high-quality JPG format</li>
+          <li style={{ fontFamily: 'Josefin Sans' }}>Email delivery 1-3 days after lesson</li>
+        </ul>
         <div style={{ width: '50%', textAlign: 'center', display: 'block', margin: '0 auto', marginTop: '2%' }}>
           <table className="table" style={{ width: '100%', textAlign: 'center' }}>
             <tbody>
               <tr>
-                <td style={{ fontWeight: 'bold', fontSize: '20px' }}>50 shots</td>
-                <td style={{ fontWeight: 'bold', fontSize: '20px' }}>$75 cash / $85 PayPal</td>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>Student Package*</td>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>15 images</td>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>$75 cash / $85 PayPal</td>
+              </tr>
+              <tr>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>Value Package</td>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>30 images</td>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>$110 cash / $120 PayPal</td>
+              </tr>
+              <tr>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>Extra Value Package</td>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>50 images</td>
+                <td style={{ fontWeight: 'bold', fontSize: '16px' }}>$160 cash / $170 PayPal</td>
               </tr>
             </tbody>
+            <p style={{ fontFamily: 'Josefin Sans', marginTop: '5%' }}>*Must show valid student ID</p>
           </table>
         </div>
       </div>
