dd10436e6d7bfbef7bb436bc5a29c8b419e8d20a
Victoria Dunham <vkurzweg@gmail.com>
2017-06-16 13:30:45 -0700

    fix mobile signup form

--- app/components/retreats/SignupModal/index.js
+++ app/components/retreats/SignupModal/index.js
@@ -31,7 +31,7 @@ function SignupModal(props) {
         contentLabel="Retreat Waiting List"
       >
         <div style={{ width: '90%', margin: '0 auto' }}>
-         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfmbOutee2SHBuyoMzrSr_dG6z46QH0rY7TmV4muK-sdxy9lg/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfmbOutee2SHBuyoMzrSr_dG6z46QH0rY7TmV4muK-sdxy9lg/viewform?embedded=true" style={{ width: '100%' }} width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
         </div>
       </Modal>
     </div>
