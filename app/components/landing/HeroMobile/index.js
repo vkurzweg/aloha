52fca0c2310e23fd74c648f9d570d30fc3c49ba2
Victoria Kurzweg <vkurzweg@gmail.com>
2017-11-16 00:02:15 -0800

    mobile redesign adjustments

--- app/components/landing/HeroMobile/index.js
+++ app/components/landing/HeroMobile/index.js
@@ -18,7 +18,8 @@ const StyledImage = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  height: 475px;
+  height: 500px;
+  margin-top: '-5%';
 `;
 
 
@@ -32,7 +33,9 @@ function Hero() {
   return (
     <div style={{ width: '100%', backgroundColor: 'black', top: '0' }}>
       <div style={{ zIndex: '10', dispay: 'block', margin: '0 auto', width: '100%', position: 'absolute' }}>
-        <h4 style={{ fontFamily: 'Josefin Sans', textAlign: 'center', color: '#F3BB58', textTransform: 'uppercase', letterSpacing: '3px', fontWeight: 'bold', marginTop: '75%', fontSize: '30px' }}>Venice Beach <br /> surf lessons <br /> $85+</h4>
+        <h4 style={{ fontFamily: 'Josefin Sans', textAlign: 'center', color: '#F3BB58', textTransform: 'uppercase', letterSpacing: '3px', fontWeight: 'bold', marginTop: '75%', fontSize: '30px' }}>Venice Beach <br /> surf lessons</h4>
+        <hr style={{ color: '#F3BB58', width: '30%', textAlign: 'center', marginTop: '-.5%' }} />
+        <h4 style={{ fontFamily: 'Josefin Sans', textAlign: 'center', color: '#F3BB58', textTransform: 'uppercase', letterSpacing: '3px', fontWeight: 'bold', fontSize: '32px', marginTop: '-1%' }}>$85+</h4>
         <Link to="/contact" style={{ textDecoration: 'none' }}><Button>Book a lesson</Button></Link>
       </div>
       <StyledImage />
